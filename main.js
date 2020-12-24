bdef977764dcf575e25c6338322f10c45acb9bc2
geeth22 <geethchandra22@gmail.com>
2020-12-24 11:05:10 +0530

    CRUD operations in AddressBook

---
+++ main.js
@@ -0,0 +1,33 @@
+const Utility = require('./utility');
+
+var readlineSync = require('readline-sync');
+
+checkoption = function () {
+    let isTerminated = true;
+    while (isTerminated == true) {
+        let checkoption = readlineSync.question("\nEner your choice:\n1.Read\n2.Add\n3.delete\n4.search\n5.update\n6.exit\n");
+        switch (checkoption) {
+            case '1':
+                Utility.readFile();
+                break;
+            case '2':
+                Utility.writeFilePromise();
+                break;
+            case '3':
+                Utility.delete();
+                break;
+            case '4':
+                Utility.search();
+                break;
+            case '5':
+                Utility.update();
+                break;
+            case '6':
+                isTerminated = false;
+                break;
+            default: console.log("please enter correct option");
+        }
+    }
+}
+checkoption();
+
